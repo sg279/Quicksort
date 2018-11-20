2a44a89ff2c9e37c77b4b880988cdb4d12091f6b
Sam <54mgold@gmail.com>
2018-11-20 22:28:27 +0000

    create lists

--- src/Quicksort.java
+++ src/Quicksort.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,11 +9,18 @@ import java.util.Random;
 
 public class Quicksort {
 
+    static ArrayList<double[]> allResults = new ArrayList<>();
+
+    /**
+     * A regular recursive quicksort implementation, choosing the pivot point to be the first element in the list
+     * @param list The list to be sorted
+     * @return The sorted list
+     */
     public static ArrayList<Integer> quicksort(ArrayList<Integer> list) {
         if (list.size() == 1) {
             return list;
         } else {
-            Integer pivot = list.remove(list.size() / 2);
+            Integer pivot = list.remove(list.size()/2);
             ArrayList<Integer> lessThan = new ArrayList<Integer>();
             ArrayList<Integer> moreThan = new ArrayList<Integer>();
             while (!list.isEmpty()) {
@@ -36,44 +46,48 @@ public class Quicksort {
     }
 
     public static void main(String[] args) {
-        ArrayList<Integer> unsorted = new ArrayList<>();
-        Random random = new Random();
-        for (int i = 0; i < 5000; i++) {
-            unsorted.add(random.nextInt(10000));
+
+        for (float i = 0; i <= 3; i++) {
+            for (int j = 100; j <= 1000; j += 100) {
+                ArrayList<Integer> list = createList(i / 20, j);
+                System.out.println(runTest(list, 5000));
+            }
+
+
+            File directory = new File(System.getProperty("user.dir") + "/results/");
+            if (!directory.isDirectory()) {
+                new File(System.getProperty("user.dir") + "/results/").mkdirs();
+            }
+            File file = new File(directory.getAbsolutePath() + "/timeComplexityMidPivotDescending.csv");
+            try {
+                FileWriter fileWriter = new FileWriter(file, true);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                //bufferedWriter.write("Sortedness, Average time");
+                bufferedWriter.write(i+"/20, Size of list, Average time");
+                bufferedWriter.newLine();
+                for (double[] results : allResults
+                ) {
+                    bufferedWriter.write(results[0] + "," + results[1]+ ","+results[2]);
+                    //bufferedWriter.write(results[0] + "," + results[1]);
+                    bufferedWriter.newLine();
+                }
+                bufferedWriter.close();
+                fileWriter.close();
+                allResults.clear();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
-        ArrayList<Integer> sorted = new ArrayList<>();
-        for (int i = 0; i < 5000; i++) {
-            sorted.add(i);
-        }
-        ArrayList<Integer> reverseSorted = new ArrayList<>();
-        for (int i = 0; i < 5000; i++) {
-            reverseSorted.add(5000 - i);
-        }
-        ArrayList<Integer> descendAscend = new ArrayList<>();
-        for (int i = 0; i < 2500; i++) {
-            descendAscend.add(2500 - i);
-        }
-        for (int i = 0; i < 2500; i++) {
-            descendAscend.add(i);
-        }
-        ArrayList<Integer> ascendDescend = new ArrayList<>();
-        for (int i = 0; i < 2500; i++) {
-            ascendDescend.add(i);
-        }
-        for (int i = 0; i < 2500; i++) {
-            ascendDescend.add(2500-i);
-        }
-        System.out.println(runTest(reverseSorted, 1000));
-        System.out.println(runTest(unsorted, 1000));
-        System.out.println(runTest(sorted, 1000));
-        System.out.println(runTest(ascendDescend, 10));
-        System.out.println(runTest(descendAscend, 10));
-        /*System.out.println(runTest(createList(0,5000),1000));
-        System.out.println(runTest(createList(0.5,5000),1000));
-        System.out.println(runTest(createList(1,5000),1000));
-*/
+
+
     }
 
+    /**
+     * This method gets the average time it takes in milleseconds to sort a list a given number of times
+     * @param list The list to be sorted
+     * @param tests The number of times to sort the list
+     * @return A string saying the size, sortedness, and average time it takes to sort the list,
+     */
     public static String runTest(ArrayList<Integer> list, int tests) {
         ArrayList<Long> timeTaken = new ArrayList<>();
         for (int i = 0; i < tests; i++) {
@@ -82,13 +96,30 @@ public class Quicksort {
             Date now = new Date();
             long start = now.getTime();
             quicksort(temp);
-            now=new Date();
+            now = new Date();
             long end = now.getTime();
             timeTaken.add(end - start);
         }
-        return "A list of " + list.size() + " items with sortedness "+sortedness(list)+" sorted " + tests + " times took on average " + averageTime(timeTaken) + "ms";
+        double averageTime = averageTime(timeTaken);
+
+        //Choosing what measure of sortedness to use
+        double sortedness = sortedness(list);
+        //double sortedness = adjacentSortedness(list);
+
+        //Choosing what results to record
+        //allResults.add(new double[]{sortedness, averageTime});
+        allResults.add(new double[]{sortedness, list.size(), averageTime});
+
+        return "A list of " + list.size() + " items with sortedness " + sortedness + " sorted " + tests + " times took on average " + averageTime + "ms";
+
+
     }
 
+    /**
+     * This method gets the mean of a list of times
+     * @param times The list of times
+     * @return The mean of the list
+     */
     public static double averageTime(ArrayList<Long> times) {
         double total = 0;
         for (Long time : times
@@ -98,18 +129,24 @@ public class Quicksort {
         return total / times.size();
     }
 
+    /**
+     * This method returns 1 minus the number of swaps bubble sort makes to sort a given list divided by the maximum number of swaps a bubble sort can
+     * make to sort a list of that size
+     * @param list The list being checked for 'sortedness'
+     * @return The 'sortedness' of the list
+     */
     public static double sortedness(ArrayList<Integer> list) {
         boolean swapsMade = true;
-        double swaps=0;
+        double swaps = 0;
         //Run until no swaps are made
         while (swapsMade) {
             swapsMade = false;
-            for (int i = 0; i < list.size()-1; i++) {
+            for (int i = 0; i < list.size() - 1; i++) {
                 //If the node after the current node is larger than the current node, swap the node's elements
-                if (list.get(i)>list.get(i+1)) {
+                if (list.get(i) > list.get(i + 1)) {
                     int temp = list.get(i);
-                    list.set(i, list.get(i+1));
-                    list.set(i+1,temp);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
                     swapsMade = true;
                     swaps++;
                 }
@@ -118,19 +155,55 @@ public class Quicksort {
         }
         int n = list.size();
 
-        return 1-(swaps/(0.5*(n-1)*n));
+        return 1 - (swaps / (0.5 * (n - 1) * n));
     }
 
-    public static ArrayList<Integer> createList(double randomness, int size){
+    /**
+     * This method returns 1 minus the number of adjacent pairs in a given list are out of order divided by the size of the list
+     * @param list The list being checked for 'sortedness'
+     * @return The 'sortedness' of the list
+     */
+    public static double adjacentSortedness(ArrayList<Integer> list) {
+        float outOfOrder = 0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            //If the node after the current node is larger than the current node, swap the node's elements
+            if (list.get(i) > list.get(i + 1)) {
+                outOfOrder++;
+            }
+
+        }
+        int n = list.size();
+
+        return 1 - (outOfOrder / n);
+    }
+
+    /**
+     * This method Creates a list of a given size and randomises a given proportion of items
+     * @param randomness The proportion of the list to be randomised
+     * @param size The size of the list
+     * @return The randomised list
+     */
+    public static ArrayList<Integer> createList(double randomness, int size) {
         ArrayList<Integer> list = new ArrayList<>();
         Random random = new Random();
+        //Create a list of descending values to 0 and then ascending values
+        /*for (int i = 0; i < size/2; i++) {
+            list.add(list.size()-i);
+        }
+        for (int i = 0; i < size/2; i++) {
+            list.add(i);
+        }*/
+        //Create a sorted list
         for (int i = 0; i < size; i++) {
             list.add(i);
         }
-        for(int i = 0; i<(randomness*size); i++){
-            list.set(random.nextInt(size), random.nextInt(size*2));
+        //Randomise twice the given randomness value numbers in the list
+        for (int i = 0; i < (randomness * size); i++) {
+            list.set(random.nextInt(size), random.nextInt(size * 2));
         }
         return list;
 
     }
+
+
 }
